3ea90e94fcc230247e01462fe71b40d6afae8594
Максим Новосельцев (НРСУБД) <novoseltsevms@consultant.ru>
2024-10-29 19:45:05 +0300

    Bugfix

--- src/app/(hub)/ya-direct/[id]/page.tsx
+++ src/app/(hub)/ya-direct/[id]/page.tsx
@@ -1,13 +1,11 @@
 import Post from '@/components/Post';
-import WidgetHelpCards from '@/components/WidgetHelpCards';
 import AuthGuard from '@/hocs/AuthGuard';
 import BuyPostGuard from '@/hocs/BuyPostGuard';
 import { getPostContent } from '@/lib/posts';
-import { Divider } from '@mui/material';
 import { redirect } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const postInfo = await getPostContent({ category: 'tilda-widgets', slug: params.id });
+  const postInfo = await getPostContent({ category: 'ya-direct', slug: params.id });
 
   if (!postInfo) return redirect('/not-found');
 
